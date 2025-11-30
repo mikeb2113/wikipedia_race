d36d000fa79297ba3ab5b5e328ee14770d5199fb
mikeb2113 <mikebermudez2113@gmail.com>
2025-11-30 17:22:41 -0500

    implementing database functions

--- project_internals/src/test/java/AppTest.java
+++ project_internals/src/test/java/AppTest.java
@@ -1,20 +1,17 @@
-package com.example;
+/*package com.example;
 
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
     }
 }
+*/
